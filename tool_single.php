3bbc6a7d20f0983a4be094dcbdad9c4a8faa3bd3
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-12-15 12:50:27 +0000

    chate_deployment_script.php

--- tool_single.php
+++ tool_single.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if ($_GET['tool']) {
   $tool = getToolInfo($_GET['tool']);
 }
@@ -11,13 +11,13 @@ if (!$tool) {
   <ul class="nav">
 
     <?php
-   
+
     foreach ($tool['inputs'] as $input => $input_data) {
       $activeClass = ($_GET['input'] == $input) ? 'active' : ''; // Check if the current input is active
       echo '<li><a href="javascript:void(0);" class="tab-title ' . $activeClass . '" data-title="title1"><svg class="icon"><use xlink:href="#' . $input_data['svg_icon_id'] . '"></use></svg>' . $input_data['title'] . '</a></li>';
     }
     ?>
-    
+
     <li>
       <a href="javascript:void(0);" class="tab-title" data-title="title8"><svg class="icon">
           <use xlink:href="#ico-7"></use>
@@ -26,7 +26,7 @@ if (!$tool) {
   </ul>
   <Br><br>
   <div class="title">WP shortcode for this form:</div>
-    <code>
+  <code>
       [sensorica_form tool_name='<?php echo $tool['slug']; ?>']
     </code>
 </div>
@@ -35,12 +35,12 @@ if (!$tool) {
 <div class="content">
   <form action="" method="post">
     <?php
-    
+
     if ($_POST['action'] == 'Finalize') {
       //if user click on finalize button, show the confrimation screen with all the inputs and it's value and submit button
       echo $tool['main_action'];
-      include("tools/".$tool['slug']."/".$tool['main_action_php_file']); //include the main action file
-      
+      include("tools/" . $tool['slug'] . "/" . $tool['main_action_php_file']); //include the main action file
+    
     } else {
       //if user click on apply and next button, save input to hidden input in use already send its value
       foreach ($tool['inputs'] as $input => $input_data) {
@@ -53,14 +53,14 @@ if (!$tool) {
       if ($_POST[$input]) {
         //save input to hidden input in use already send its value
         echo '<input type="hidden" name="' . $input . '" value="' . $_POST[$input] . '" />';
-        
+
         //if this input is the last one show the confrimation screen with all the inputs and it's value and submit button
         if ($input == end(array_keys($tool['inputs']))) {
           foreach ($tool['inputs'] as $input => $input_data) {
             echo '<div class="form-section"><label for="' . $input . '">' . $input_data['description'] . '</label><input type="text" class="form-control" name="' . $input . '" id="' . $input . '" value="' . $_POST[$input] . '" disabled /></div>';
           }
           echo '<input type="submit" value="Finalize" name="action" class="btn" />';
-          
+
         }
         //if this input is not the last one, continue
         continue;
@@ -75,105 +75,158 @@ if (!$tool) {
           <?php echo $input_data['title']; ?>
         </div>
         <?php
-        
-          if ($input_data['textarea'] == true) {
-            
-            include("toole_single_prompt_textarea.php");
-          } else { ?>
-        <div class="form-section">
-          <label for="<?php echo $input; ?>">
-            <?php echo $input_data['description']; ?>
-          </label>
 
-          
-          
-          
-          <?php 
-          if ($input_data['magic_prompt']) { 
-            //add small icon to form-control input to the right with link to the magic prompt
-            ?>
-            <div>
-          <input class="form-control userinput" type="text" name="<?php echo $input; ?>" id="<?php echo $input; ?>"
-            placeholder="<?php echo $input_data['placeholder']; ?>" />
+        if ($input_data['textarea'] == true) {
 
-          <div class="magic-prompt">
-            <span alt="Generate" title="Generate">🪄</span>
-          </div>
-          </div>
-          <style>
-            .magic-prompt {
-              
-              left: -40px;
-              top: -40px;
-              position: relative;
-              width: 40px;
-              color: #fff;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              cursor: pointer;
-            }
-            </style>
-            <script>
-              //call ajax.php?magic_prompt=1&userinput= and put value instead of userinput
-              $(document).ready(function () {
-                $('.magic-prompt').click(function () {
-                  var userinput = $(this).parent().find('.userinput').val();
-                  $.ajax({
-                    url: "ajax.php?magic_prompt=1&userinput=" + userinput,
-                    //show 'stop' icon in .magic-prompt while loading
-                    beforeSend: function () {
-                      $('.magic-prompt').html('⏹');
-                    },
-                    //show '🪄' icon in .magic-prompt after loading
-                    complete: function () {
-                      $('.magic-prompt').html('🪄');
-                    },
-                    success: function (result) {
-                      //json
-                      var obj = JSON.parse(result);
-                      //put the result in the input
-                      $('.userinput').val(obj.message);
-                    }
-                  });
-                });
-              });
-            </script>
+          include("toole_single_prompt_textarea.php");
+        } else { ?>
+          <div class="form-section">
+            <label for="<?php echo $input; ?>">
+              <?php echo $input_data['description']; ?>
+            </label>
+
             <?php
-          } else {
-            ?>
-            <input type="text" class="form-control" name="<?php echo $input; ?>" id="<?php echo $input; ?>"
-            placeholder="<?php echo $input_data['placeholder']; ?>" />
-            <?php
-          }
-          ?>
-          
+            if ($input_data['magic_prompt']) {
+              //add small icon to form-control input to the right with link to the magic prompt
+              ?>
+              <div>
+                <input class="form-control userinput" type="text" name="<?php echo $input; ?>" id="<?php echo $input; ?>"
+                  placeholder="<?php echo $input_data['placeholder']; ?>" />
 
-          <details>
-            <summary>
-              Instructions
-              <div class="chevron">
-                <svg class="icon">
-                  <use xlink:href="#chevron"></use>
-                </svg>
+                <div class="magic-prompt">
+                  <span alt="Generate" title="Generate"><svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M20.7 11.38C16.13 12.53 12.53 16.12 11.38 20.7C11.28 21.1 10.71 21.1 10.61 20.7C9.47 16.13 5.87 12.53 1.3 11.38C0.9 11.28 0.9 10.71 1.3 10.61C5.87 9.47 9.47 5.87 10.62 1.3C10.72 0.9 11.29 0.9 11.39 1.3C12.54 5.87 16.13 9.47 20.71 10.62C21.11 10.72 21.11 11.29 20.71 11.39L20.7 11.38ZM30.82 24.77C28.08 24.08 25.92 21.92 25.23 19.18C25.17 18.94 24.83 18.94 24.77 19.18C24.08 21.92 21.92 24.08 19.18 24.77C18.94 24.83 18.94 25.17 19.18 25.23C21.92 25.92 24.08 28.08 24.77 30.82C24.83 31.06 25.17 31.06 25.23 30.82C25.92 28.08 28.08 25.92 30.82 25.23C31.06 25.17 31.06 24.83 30.82 24.77Z"
+                        fill="#94ABCF" />
+                    </svg>
+                  </span>
+                </div>
               </div>
-            </summary>
-            <div class="summary-content">
-              <ol>
-                <?php
+              <style>
+                .magic-prompt {
 
-                foreach ($input_data['help_image'] as $image) {
-                  echo '<li>' . $image . '</li>';
+                  left: -40px;
+                  top: -40px;
+                  position: relative;
+                  width: 40px;
+                  color: #fff;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  cursor: pointer;
                 }
 
-                ?>
-              </ol>
-            </div>
-          </details>
-        </div>
-        <input type="submit" value="Apply & Next" class='btn' />
-        <?php } ?>      
-        
+                .fade-out {
+                  animation: fadeOut 0.1s;
+                  opacity: 0;
+                }
+
+                .fade-in {
+                  animation: fadeIn 0.1s;
+                  opacity: 1;
+                }
+
+                @keyframes fadeOut {
+                  from {
+                    opacity: 1;
+                  }
+
+                  to {
+                    opacity: 0;
+                  }
+                }
+
+                @keyframes fadeIn {
+                  from {
+                    opacity: 0;
+                  }
+
+                  to {
+                    opacity: 1;
+                  }
+                }
+              </style>
+              <script>
+                //call ajax.php?magic_prompt=1&userinput= and put value instead of userinput
+                $(document).ready(function () {
+                  $('.magic-prompt').click(function () {
+                    var userinput = $(this).parent().find('.userinput').val();
+                    $.ajax({
+                      url: "ajax.php?magic_prompt=1&userinput=" + userinput,
+                      //show 'stop' icon in .magic-prompt while loading
+                      beforeSend: function () {
+                        var magicPrompt = $('.magic-prompt');
+                        magicPrompt.removeClass('fade-in').addClass('fade-out');
+
+                        // Wait for the fade-out animation to complete 
+                        setTimeout(function () {
+                          magicPrompt.html(`<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 3C23.17 3 29 8.83 29 16C29 23.17 23.17 29 16 29C8.83 29 3 23.17 3 16C3 8.83 8.83 3 16 3ZM16 1C7.72 1 1 7.72 1 16C1 24.28 7.72 31 16 31C24.28 31 31 24.28 31 16C31 7.72 24.28 1 16 1ZM21 19.5V12.5C21 11.67 20.33 11 19.5 11H12.5C11.67 11 11 11.67 11 12.5V19.5C11 20.33 11.67 21 12.5 21H19.5C20.33 21 21 20.33 21 19.5Z" fill="#94ABCF"/>
+      </svg>`); // Your 'stop' icon SVG here
+                          magicPrompt.removeClass('fade-out').addClass('fade-in');
+                        }, 100);
+                      },
+
+                      complete: function () {
+                        var magicPrompt = $('.magic-prompt');
+                        magicPrompt.removeClass('fade-in').addClass('fade-out');
+
+                        // Wait for the fade-out animation to complete 
+                        setTimeout(function () {
+                          magicPrompt.html(`<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20.7 11.38C16.13 12.53 12.53 16.12 11.38 20.7C11.28 21.1 10.71 21.1 10.61 20.7C9.47 16.13 5.87 12.53 1.3 11.38C0.9 11.28 0.9 10.71 1.3 10.61C5.87 9.47 9.47 5.87 10.62 1.3C10.72 0.9 11.29 0.9 11.39 1.3C12.54 5.87 16.13 9.47 20.71 10.62C21.11 10.72 21.11 11.29 20.71 11.39L20.7 11.38ZM30.82 24.77C28.08 24.08 25.92 21.92 25.23 19.18C25.17 18.94 24.83 18.94 24.77 19.18C24.08 21.92 21.92 24.08 19.18 24.77C18.94 24.83 18.94 25.17 19.18 25.23C21.92 25.92 24.08 28.08 24.77 30.82C24.83 31.06 25.17 31.06 25.23 30.82C25.92 28.08 28.08 25.92 30.82 25.23C31.06 25.17 31.06 24.83 30.82 24.77Z" fill="#94ABCF"/>
+      </svg>`); // Your '🪄' icon SVG here
+                          magicPrompt.removeClass('fade-out').addClass('fade-in');
+                        }, 200);
+                      },
+
+
+                      success: function (result) {
+                        //json
+                        var obj = JSON.parse(result);
+                        //put the result in the input
+                        $('.userinput').val(obj.message);
+                      }
+                    });
+                  });
+                });
+              </script>
+              <?php
+            } else {
+              ?>
+              <input type="text" class="form-control" name="<?php echo $input; ?>" id="<?php echo $input; ?>"
+                placeholder="<?php echo $input_data['placeholder']; ?>" />
+              <?php
+            }
+            ?>
+
+
+            <details>
+              <summary>
+                Instructions
+                <div class="chevron">
+                  <svg class="icon">
+                    <use xlink:href="#chevron"></use>
+                  </svg>
+                </div>
+              </summary>
+              <div class="summary-content">
+                <ol>
+                  <?php
+
+                  foreach ($input_data['help_image'] as $image) {
+                    echo '<li>' . $image . '</li>';
+                  }
+
+                  ?>
+                </ol>
+              </div>
+            </details>
+          </div>
+          <input type="submit" value="Apply & Next" class='btn' />
+        <?php } ?>
+
       </div>
       <?php
       //show only one setting per step
---
+++ tools/chate/deploy_chate_to_vercel.php
@@ -0,0 +1,309 @@
+<?php
+function esc_attr($v)
+{
+    return $v;
+}
+function esc_url($v)
+{
+    return $v;
+}
+
+// Define your environment variables
+$nextPublicDefaultSystemPrompt = $_POST['NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT'];
+$openaiApiKey = $_POST['OPENAI_API_KEY'];
+$mainTitle = $_POST['main_title'];
+$yourVercelToken = $_POST['YOUR_VERCEL_TOKEN']; // Assuming 'YOUR_VERCEL_TOKEN' is the key in the POST request
+$githubAccessToken = $_POST['githubAccessToken']; // Replace with your GitHub access token
+
+// Main logic
+$repoFullName = 'noot/op-deposit';
+
+$projectNameVercel = 'chate';
+
+
+
+
+
+
+if (empty($githubAccessToken)) {
+    echo "Please provide a GitHub access token.";
+    die();
+}
+// Function to make a cURL request
+function makeCurlRequest($url, $githubAccessToken, $postData = null, $customRequest = 'POST')
+{
+    $headers = [
+        "Accept: application/vnd.github+json",
+        "Authorization: Bearer $githubAccessToken",
+        "User-Agent: your-app-name" // Replace with your app name
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $customRequest);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    if (!empty($postData)) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+    }
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Curl error: ' . curl_error($ch);
+    }
+    curl_close($ch);
+
+    return json_decode($response, true);
+}
+
+
+// Fork a repository
+
+
+$url = "https://api.github.com/repos/$repoFullName/forks";
+echo $url;
+$org = explode("/", $repoFullName);
+$repo = $org[1];
+$org = $org[0];
+$postData = [
+    "owner" => $org, // Replace with the actual organization name if needed
+    "name" => $repo,     // Replace with the desired repository name
+    "default_branch_only" => true
+];
+
+echo "Repository '$repo' forking...\n";
+$response = makeCurlRequest($url, $githubAccessToken, $postData);
+
+if ($response['error']) {
+    echo "Error: " . $response['error']['message'];
+    die();
+}
+
+
+
+if (isset($response['owner']['login'])) {
+    echo "Forked successfully. Owner: " . $response['owner']['login'];
+} else {
+    echo "Forking failed.";
+    die();
+}
+
+$repo = $response['name'];
+$owner = $response['owner']['login'];
+
+if ($debug == true)
+    print_r($response);
+if ($debug == true)
+    echo "\n";
+
+
+
+
+
+function deleteVercelProject($projectNameVercel, $vercelToken)
+{
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => "https://api.vercel.com/v9/projects/$projectNameVercel",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        CURLOPT_HTTPHEADER => [
+            "Authorization: Bearer $vercelToken"
+        ],
+    ]);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        // Handle cURL error here
+    }
+    curl_close($ch);
+
+    return $response;
+}
+
+
+
+// Step 1: Delete existing project (if it exists)
+$deleteResponse = deleteVercelProject($projectNameVercel, $yourVercelToken);
+
+if ($debug == true) {
+    print "deleteResponse:\n";
+    print_r($deleteResponse);
+}
+
+echo "<br>vercel project deleted (if exists)<br>";
+// cURL setup
+$ch = curl_init();
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_URL, "https://api.vercel.com/v9/projects");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+
+// Prepare the body of the POST request
+$body = json_encode(array(
+    "name" => $projectNameVercel,
+    "buildCommand" => "next build",
+    "devCommand" => "next dev --port $PORT",
+    "environmentVariables" => array(
+        array(
+            "key" => "NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT",
+            "value" => $nextPublicDefaultSystemPrompt,
+            "type" => "plain",
+            "target" => "production"
+        ),
+        array(
+            "key" => "OPENAI_API_KEY",
+            "value" => $openaiApiKey,
+            "type" => "plain",
+            "target" => "production"
+        ),
+        array(
+            "key" => "main_title",
+            "value" => $mainTitle,
+            "type" => "plain",
+            "target" => "production"
+        )
+    ),
+    "framework" => "nextjs",
+    "gitRepository" => array(
+        "repo" => $owner . "/" . $repo,
+        "type" => "github"
+    ),
+    "publicSource" => true,
+    "skipGitConnectDuringLink" => true
+));
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+
+// Set HTTP Header for POST request
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Authorization: Bearer $yourVercelToken",
+    "Content-Type: application/json"
+));
+
+
+// Execute cURL session
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    // Handle cURL error here
+}
+
+// Close cURL session
+curl_close($ch);
+
+$json = json_decode($response, true);
+
+
+if ($json['error']) {
+    echo "Error add project: " . $json['error']['message'];
+    die();
+}
+
+if ($json['id']) {
+    echo "Project added successfully. ID: " . $json['id'];
+} else {
+    echo "Project adding failed.";
+    die();
+}
+
+// 3 trigger ci
+function createNewFile($owner, $repo, $path, $message, $content, $githubAccessToken, $sha = null)
+{
+    $url = "https://api.github.com/repos/$owner/$repo/contents/$path";
+    $data = [
+        'message' => $message,
+        'content' => base64_encode($content)
+    ];
+
+    // Add sha if it's provided (for updating an existing file)
+    if ($sha !== null) {
+        $data['sha'] = $sha;
+    }
+
+    return makeCurlRequest($url, $githubAccessToken, $data, 'PUT');
+}
+
+// Usage
+$filePath = time() . '.txt'; // Replace with your desired file name
+$commitMessage = 'Add new file'; // Your commit message
+$fileContent = time(); // The content of the new file
+
+// If updating an existing file, provide the SHA. Otherwise, omit it or pass null.
+$response = createNewFile($owner, $repo, $filePath, $commitMessage, $fileContent, $githubAccessToken, $sha /* or null for a new file */);
+
+echo "File creation response:\n";
+if ($debug == true)
+    print_r($response);
+
+// Check success of file creation
+if (isset($response['content']) && $response['content']['path'] == $filePath) {
+    echo "File created successfully.";
+} else {
+    echo "File creation failed.";
+    print_r($response);
+    die();
+}
+
+
+$debug = true;
+
+
+
+echo "check deployments\n";
+
+function getDeploymentDomain($projectNameVercel, $yourVercelToken)
+{
+    sleep(10);
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => "https://api.vercel.com/v9/projects/$projectNameVercel",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            "Authorization: Bearer $yourVercelToken"
+        ],
+    ]);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error: ' . curl_error($ch);
+    } else {
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($httpCode == 200) {
+            $data = json_decode($response, true); // Decode JSON response
+            print_r($data);
+            if (isset($data['latestDeployments']) && is_array($data['latestDeployments'])) {
+                foreach ($data['latestDeployments'] as $deployment) {
+                    if (isset($deployment['alias']) && is_array($deployment['alias'])) {
+                        foreach ($deployment['alias'] as $alias) {
+                            return $alias;
+                        }
+                    }
+                }
+            }
+        } else {
+            echo "HTTP Error Code: " . $httpCode;
+        }
+    }
+    curl_close($ch);
+
+    return null;
+}
+
+if (true) {
+    $alias = getDeploymentDomain($projectNameVercel, $yourVercelToken);
+} else {
+    $alias = "https://chate-{$owner}.vercel.app";
+}
+if (!empty($alias)) {
+    echo "Deployment domain: " . $alias;
+} else {
+    echo "No deployment found.";
+    die();
+}
+
+
+die();
+?>
